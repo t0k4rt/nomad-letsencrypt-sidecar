fc9a995a8a5c2956a2c3e3e7f0c1689484d5ee89
Alexandre Assouad <assouad@kairosfire.com>
2023-10-10 16:18:57 +0200

    minor change

--- config/haproxy_hook.sh
+++ config/haproxy_hook.sh
@@ -9,9 +9,9 @@ upload_cert() {
     DOMAIN="$1"
     echo "Make full cert for $DOMAIN"
     cat "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" "/etc/letsencrypt/live/$DOMAIN/privkey.pem" > "/$DEST_DIR/$DOMAIN.pem"
-    echo "Upload cert $DOMAIN to consul"
-    # echo "consul kv put HAPROXY_$DOMAIN @$DEST_DIR/$DOMAIN.pem"
-    if [ ! -z "$CONSUL_HTTP_ADDR" ]; then
+    echo "Upload cert for $DOMAIN to consul"
+
+    if [ -n "$CONSUL_HTTP_ADDR" ]; then
         echo "Will use consul endpoint: $CONSUL_HTTP_ADDR"
     fi
     consul kv put "HAPROXY_$DOMAIN" "@$DEST_DIR/$DOMAIN.pem"
